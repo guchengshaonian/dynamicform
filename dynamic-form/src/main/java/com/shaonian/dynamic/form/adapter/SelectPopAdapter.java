078974f1cca655f13eb55336f947869a67029c64
wk <wankemie@163.com>
2021-05-07 17:55:54 +0800

    完成popwindow

---
+++ dynamic-form/src/main/java/com/shaonian/dynamic/form/adapter/SelectPopAdapter.java
@@ -0,0 +1,123 @@
+package com.shaonian.dynamic.form.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.shaonian.dynamic.form.R;
+import com.shaonian.dynamic.form.bean.DictBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * 下拉选择框的adapter
+ *
+ * @author wk
+ * @date 2021/05/07
+ */
+
+public class SelectPopAdapter extends RecyclerView.Adapter<SelectPopAdapter.SelectPopViewHolder> {
+
+    private final List<DictBean> mDictBeans;
+    private final List<DictBean> mChooseBeans = new ArrayList<>();
+    private final boolean isMultiple;
+    private final boolean isLinked;
+    private SelectValueListener mSelectValueListener;
+
+    public void setSelectValueListener(SelectValueListener selectValueListener) {
+        mSelectValueListener = selectValueListener;
+    }
+
+    public interface SelectValueListener {
+        /**
+         * 单选的选择内容
+         *
+         * @param dictBean 选中的内容
+         */
+        void onSelectValue(DictBean dictBean);
+    }
+
+    public List<DictBean> getChooseBeans() {
+        return mChooseBeans;
+    }
+
+    public SelectPopAdapter(List<DictBean> dictBeanList, boolean isMultiple, boolean isLinked) {
+        super();
+        this.mDictBeans = dictBeanList;
+        this.isMultiple = isMultiple;
+        this.isLinked = isLinked;
+    }
+
+    public void setNewData(List<DictBean> dictBeans) {
+        mDictBeans.clear();
+        mDictBeans.addAll(dictBeans);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public SelectPopViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SelectPopViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_select_pop, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SelectPopAdapter.SelectPopViewHolder holder, int position) {
+        holder.fill(mDictBeans.get(position), isMultiple, isLinked);
+        holder.itemView.setOnClickListener(v -> {
+            if (isMultiple) {
+                if (isLinked) {
+                    // TODO 展开下一层
+                } else {
+                    mChooseBeans.add(mDictBeans.get(position));
+                }
+            } else {
+                if (isLinked) {
+                    // TODO 展开下一层
+                } else {
+                    if (mSelectValueListener != null) {
+                        mSelectValueListener.onSelectValue(mDictBeans.get(position));
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDictBeans.size();
+    }
+
+    public static class SelectPopViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView ivArrow;
+        private final ImageView ivChoose;
+        private final TextView tvValue;
+
+        public SelectPopViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivArrow = itemView.findViewById(R.id.iv_arrow);
+            ivChoose = itemView.findViewById(R.id.iv_choose);
+            tvValue = itemView.findViewById(R.id.tv_item_value);
+        }
+
+        public void fill(DictBean dictBean, boolean isMultiple, boolean isLinked) {
+            tvValue.setText(dictBean.getName());
+            if (isMultiple) {
+                ivChoose.setVisibility(View.VISIBLE);
+            } else {
+                ivChoose.setVisibility(View.GONE);
+            }
+            if (isLinked) {
+                ivArrow.setVisibility(View.VISIBLE);
+            } else {
+                ivArrow.setVisibility(View.GONE);
+            }
+        }
+    }
+}
--- dynamic-form/src/main/java/com/shaonian/dynamic/form/widget/SelectPopWindow.java
+++ dynamic-form/src/main/java/com/shaonian/dynamic/form/widget/SelectPopWindow.java
@@ -1,7 +1,20 @@
 package com.shaonian.dynamic.form.widget;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.shaonian.dynamic.form.R;
+import com.shaonian.dynamic.form.bean.DictBean;
+import com.shaonian.dynamic.form.formitem.PullSelectFormItem;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * 下拉框的popWindow
@@ -24,27 +37,82 @@ public class SelectPopWindow extends PopupWindow {
      */
     public static final int CUSTOM = 2;
 
+    private final Context mContext;
+    private int mMode;
+    private int mStyleMode;
+    private List<DictBean> mDictBeans;
+
     /**
      * 构造函数
      *
-     * @param context   上下文
-     * @param mode      {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#SINGLE_SELECT},
-     *                  {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#MULTIPLE_SELECT},
-     *                  {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#LINKED_SINGLE_SELECT},
-     *                  {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#LINKED_MULTIPLE_SELECT},
-     * @param styleMode {@link #WARP_CONTENT}, {@link #AUTO},{@link #CUSTOM}
+     * @param context      上下文
+     * @param mode         {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#SINGLE_SELECT},
+     *                     {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#MULTIPLE_SELECT},
+     *                     {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#LINKED_SINGLE_SELECT},
+     *                     {@link com.shaonian.dynamic.form.formitem.PullSelectFormItem#LINKED_MULTIPLE_SELECT},
+     * @param styleMode    {@link #WARP_CONTENT}, {@link #AUTO},{@link #CUSTOM}
+     * @param dictBeanList 数据
      */
-    public SelectPopWindow(Context context, int mode, int styleMode) {
+    public SelectPopWindow(Context context, int mode, int styleMode, List<DictBean> dictBeanList) {
         super(context);
-        setView(mode);
+        this.mMode = mode;
+        this.mStyleMode = styleMode;
+        this.mDictBeans = dictBeanList;
+        this.mContext = context;
+        setView();
     }
 
     /**
      * 根据不同的类型展示不同的界面
-     *
-     * @param mode
      */
-    private void setView(int mode) {
+    private void setView() {
+        int layoutId;
+        switch (mMode) {
+            case PullSelectFormItem.SINGLE_SELECT:
+            case PullSelectFormItem.LINKED_SINGLE_SELECT:
+            default:
+                layoutId = R.layout.pop_view_select_single;
+                break;
+            case PullSelectFormItem.MULTIPLE_SELECT:
+            case PullSelectFormItem.LINKED_MULTIPLE_SELECT:
+                layoutId = R.layout.pop_view_select_multiple;
+                break;
+        }
+        View root = LayoutInflater.from(mContext).inflate(layoutId, null, false);
+        setContentView(root);
+        initView(root);
+    }
+
+    private void initView(View root) {
+        LinearLayout container = root.findViewById(R.id.ll_container);
+        TextView sure = root.findViewById(R.id.tv_sure);
+        if (sure != null) {
+            sure.setOnClickListener(this::chooseData);
+        }
+        // 根据选择类型以及数据源的情况动态生成recycleView并设置数据
+        switch (mMode) {
+            case PullSelectFormItem.SINGLE_SELECT:
+            case PullSelectFormItem.MULTIPLE_SELECT:
+                setSingleView(container);
+                break;
+        }
+    }
+
+    /**
+     * 设置单个的recycleView
+     *
+     * @param container 容器View
+     */
+    private void setSingleView(LinearLayout container) {
+        RecyclerView recyclerView = new RecyclerView(mContext);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+    }
+
+    /**
+     * 确定多选的内容
+     */
+    private void chooseData(View view) {
 
     }
 }
